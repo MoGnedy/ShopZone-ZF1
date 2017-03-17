9974b49811dd60ecf8a4d809dab31a4d09e87e6d
elmahdy-intake37 <elmahdy30@gmail.com>
2017-03-17 17:05:07 +0200

    index wiz category and product and details of product na2s bs 7tt hndlha lyha

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,8 +11,40 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+         $category_model= new Application_Model_Category();
+        $this->view->category = $category_model->listAll();
+    }
+
+    public function listCategoryAction()
+    {
+        // action body
+        $category_model= new Application_Model_Category();
+        $this->view->category = $category_model->listAll();
+    }
+
+    public function detailsProductAction()
+    {
+        // action body
+        $product_model=new Application_Model_Product();
+        $product_id = $this->_request->getParam("uid");
+        $product_data = $product_model->ProductDetails($product_id);
+        $this->view->product_data=$product_data;
+
+    }
+
+    public function listProductAction()
+    {
+        // action body
+         $product_model=new Application_Model_Product();
+        $this->view->product =  $product_model->listProducts();
     }
 
 
 }
+
+
+
+
+
+
 

362bf3ca76d3a0e882a0d398901f404ae2ec70fb
abdelrahmanElhoshy <wizardooo88888@gmail.com>
2017-03-18 21:02:46 +0200

    calender function

--- application/forms/Offer.php
+++ application/forms/Offer.php
@@ -1,22 +1,38 @@
 <?php
 
-class Application_Form_Offer extends Zend_Form
+class Application_Form_Offer extends ZendX_JQuery_Form
 {
 
+    //ZendX_JQuery_Form
     public function init()
     {
 
     	$this->setMethod('POST');
+   
+
+//    $textElement = new ZendX_JQuery_Form_Element_DatePicker('dtPicker', array("label" => "testtest"." (yyyy-mm-dd)"));
+//    $textElement->setJQueryParams(array(
+//    'dateFormat'=>'yy-mm-dd',
+//    'changeMonth'=> true,
+//    'changeYear'=> true
+//    ))
+//    ->setDecorators(array(
+//    array('UiWidgetElement', array('tag' => '')),
+//    array('Errors'),
+//    array('HtmlTag', array('tag' => 'div', 'class'=>'span-11 last')),
+//    array('Label', array('tag' => 'div', 'class'=>'span-5 clear'))
+//    ));
+//        
 
     	$offer_per = new Zend_Form_Element_Text('offer_per');
     	$offer_per->setLabel('Offer percentage: ');
     	$offer_per->setRequired();
-    	$offer_per->setAttribs(Array('placeholder' => 'Example:10%', 'class' => 'form-control'));
+    	$offer_per->setAttribs(Array('placeholder' => 'Example:0.10', 'class' => 'form-control'));
 
     	$offer_start = new Zend_Form_Element_Text('offer_start');
     	$offer_start->setLabel('Start date: ');
     	$offer_start->setRequired();
-    	$offer_start->setAttribs(Array('placeholder' => 'Example:2000-12-31', 'class' => 'form-control'));
+    	$offer_start->setAttribs(Array('placeholder' => 'Example:2000-12-31', 'class' => 'form-control','type' => 'date'));
 
     	$offer_end = new Zend_Form_Element_Text('offer_end');
     	$offer_end->setLabel('End date: ');
@@ -37,7 +53,8 @@ class Application_Form_Offer extends Zend_Form
 	 $offer_start,
 	 $offer_end,
 	 $submit,
-	 $reset
+	 $reset,
+         $textElement
 	));
 
         /* Form Elements & Other Definitions Here ... */

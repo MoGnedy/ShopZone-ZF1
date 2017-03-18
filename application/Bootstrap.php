b1e996e333beb38d9c94bd9d9c499e3268c6aa95
MoGnedy <mognedy@gmail.com>
2017-03-18 22:47:28 +0200

    fix Bootstrap.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -14,23 +14,3 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
   }
 
 }
-
-function _initViewHelpers()
-{
-$view = new Zend_View();
-//ZendX_JQuery_View_Helper_JQuery::enableNoConflictMode();
-ZendX_JQuery::enableView($view);
-
-$viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
-$viewRenderer->setView($view);
-Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
-
-$this->bootstrap('layout');
-$layout = $this->getResource('layout');
-$view = $layout->getView();
-
-$view->doctype('HTML5');
-$view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
-$view->headTitle()->setSeparator(' - ');
-$view->headTitle('ZendX Example');
-}

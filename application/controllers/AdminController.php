c101e405d29fcba1eea004066255cc6d71ebea0a
abdelrahmanElhoshy <wizardooo88888@gmail.com>
2017-03-23 21:55:19 +0200

    layout issues

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -267,7 +267,113 @@ class AdminController extends Zend_Controller_Action
         }
     }
 
+     public function addproductAction()
+    {
+        $form = new Application_Form_Productform();
+        $form->setAttrib('enctype', 'multipart/form-data');
+        $this->view->productForm = $form;
+        $request = $this->getRequest();
+        if($request->isPost()){
+        if($form->isValid($request->getPost())){
+        $product_model = new Application_Model_Product();
+        
+        $location = $form->picture->getFileName();
+        //print_r($location);
+        $request->setParam('picture', $location);
+        
+        $values = $form->getValues();
+ 
 
+        if (!$form->picture->receive()) {
+            print "Upload error";
+        }
+        $product_model->addNewProduct($request->getParams());
+        $this->redirect('/shop/listproducts');
+                                                }
+                              }
+        
+        
+        
+        
+    }
+
+    public function listproductsAction()
+    {   
+        $product_model = new Application_Model_Product();
+        
+        $this->view->products = $product_model->listProducts();
+        
+        
+    }
+    
+    public function productdetailsAction()
+    {
+      $product_model = new Application_Model_Product();
+      $comment_form = new Application_Form_Commentform();
+      $this->view->comment_form = $comment_form;
+      $p_id = $this->_request->getParam('pid');
+      $product = $product_model->productDetails($p_id);
+      $this->view->comments = $product_model->listProductcomments($p_id);
+      $this->view->product = $product[0];
+      $this->view->allcoments = $product_model->SelectionComment($p_id);
+      $request = $this->getRequest();
+      if($request->isPost()){
+      if($comment_form->isValid($request->getPost())){
+      $comment_model = new Application_Model_Comment();
+      $pid=$request->getParam('pid');
+      $request->setParam('product', $pid);
+      
+      
+      $request->setParam('customer_id', 1);
+
+      
+      $comment_model->addComment ($request->getParams());
+      $this->redirect("/shop/productdetails/pid/$pid");
+      
+      
+      }
+      }
+    }
+
+     public function productstaticAction()
+    {
+
+          $product_model = new Application_Model_Product();
+          $id = $this->_request->getParam('pid');
+        
+        $product = $product_model->productDetails($id);
+        $static = $product_model->sql($id);
+
+        $this->view->product = $product[0];
+        $this->view->statics = $static;
+    }
+
+    public function deleteproductAction()
+    {
+      $product_model = new Application_Model_Product();
+      $p_id = $this->_request->getParam('pid');
+      $user = $product_model->deleteProduct($p_id);
+      $this->redirect("/shop/listproducts");
+    }
+
+    public function editproductAction()
+    {
+        $form = new Application_Form_Productform ();
+        $product_model = new Application_Model_Product ();
+        $id = $this->_request->getParam('pid');
+        $product_data = $product_model->productDetails($id)[0];
+        $form->populate($product_data);
+        $this->view->product_form = $form;
+        $request = $this->getRequest();
+        if($request->isPost()){
+        if($form->isValid($request->getPost())){
+        $product_model->updateProduct($id, $_POST);
+        $this->redirect('/shop/listproducts');
+        }
+        }
+
+    }
+    
 }
         
 
